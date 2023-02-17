f3d6bcf395873479b750323ab8bf715b7fab387d
mitsuruyamaura <komayou326@gmail.com>
2023-02-17 11:43:44 +0900

    PlayerJump.cs 修正。UniRx 以外の処理で機能するように処理を追加。 NavMesh エリア外にジャンプした際に強制的に最短エリア内に戻す機能を追加。

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -13,17 +13,20 @@ using UnityEngine.AI;
 public class PlayerJump : MonoBehaviour
 {
     private NavMeshAgent agent;
+    
+    // 不要
     // private Rigidbody rb;
     // private CapsuleCollider capsuleCol;
 
     [SerializeField] private float jumpPower = 3.0f;
-    //private bool isJump = false;
 
+    
     private void Reset() {
          if (!TryGetComponent(out agent)) {
              Debug.Log("NavMeshAgent 取得出来ませんでした。");
          }
     
+         // 不要
          // if (!TryGetComponent(out rb)) {
          //     Debug.Log("Rigidbody 取得出来ませんでした。");
          // }
@@ -36,51 +39,89 @@ public class PlayerJump : MonoBehaviour
     void Start() {
         Reset();
 
-        this.UpdateAsObservable()
-            .Where(_ => agent.enabled)  // 重複ジャンプ防止
-            //.Where(_ => !isJump)
-            .Where(_ => Input.GetKeyDown(KeyCode.Space))
-            .Subscribe(_ =>
-            {
-                //isJump = true;
-                
-                // NavMesh を切らないと柵や岩などを飛び越せず前の位置に戻る。建物の場合にはいいが、飛び越えられそうなものも飛び越せなくなる
-                agent.enabled = false;
-                float y = transform.position.y;
-                
-                // その場合、大きな建物やステージ外側にコライダーつけて、isTrigger オフで対応
-                //    =>  ステージ外については SamplePosition で対応可能
-                
-                //capsuleCol.isTrigger = false;
-                Sequence sequence = DOTween.Sequence();
+        // UniRx 正常動作確認済
+        // this.UpdateAsObservable()
+        //     .Where(_ => agent.enabled)  // 重複ジャンプ防止
+        //     .Where(_ => Input.GetKeyDown(KeyCode.Space))
+        //     .Subscribe(_ =>
+        //     {
+        //         // NavMesh を切らないと柵や岩などを飛び越せず前の位置に戻る。建物の場合にはいいが、飛び越えられそうなものも飛び越せなくなる
+        //         agent.enabled = false;
+        //         float y = transform.position.y;
+        //         
+        //         // その場合、大きな建物やステージ外側にコライダーつけて、isTrigger オフで対応
+        //         //    =>  ステージ外については SamplePosition で対応可能
+        //         //capsuleCol.isTrigger = false;  // 現状は使っていない
+        //
+        //         Sequence sequence = DOTween.Sequence();
+        //
+        //         // Rigidbody だと NavMesh 下にあるので、NavMesh をオフにしないとジャンプしない
+        //
+        //         sequence.Append(transform.DOJump(new Vector3(Input.GetAxis("Horizontal") * 7, 3.0f, Input.GetAxis("Vertical") * 7), jumpPower, 1, 0.5f)
+        //             .SetRelative());
+        //
+        //         // こっちは SetRelative いらない。
+        //         sequence.Append(transform.DOMoveY(y, 0.25f)
+        //             .SetEase(Ease.OutQuad)).OnComplete(() =>
+        //         {
+        //             agent.enabled = true;
+        //             
+        //             // NavMesh のエリア外(ステージの外側)に出ていないか判定
+        //             if (NavMesh.SamplePosition(transform.position, out NavMeshHit hit, 1.0f, NavMesh.AllAreas))
+        //             {
+        //                 // エリア外の場合には最も近い位置 NavMesh の位置に再移動して位置補正
+        //                 transform.position = hit.position;
+        //             }
+        //         });
+        //         
+        //         // ここは不要
+        //         //.OnComplete(() =>
+        //         //{
+        //         //    rb.useGravity = true;
+        //         //    DOVirtual.DelayedCall(0.5f, () => agent.enabled = true);
+        //         //    rb.DOMoveY(y, 0.5f).SetEase(Ease.OutQuad).SetRelative().OnComplete(() => agent.enabled = true);
+        //         //    agent.enabled = true;
+        //         //    });
+        //     });
+    }
 
-                // Rigidbody だと NavMesh 下にあるので、NavMesh をオフにしないとジャンプしない
 
-                sequence.Append(transform.DOJump(new Vector3(Input.GetAxis("Horizontal") * 7, 3.0f, Input.GetAxis("Vertical") * 7), jumpPower, 1, 0.5f)
-                    .SetRelative());
-                //sequence.Append(transform.DOMoveY(3.0f, 0.5f).SetEase(Ease.InQuad).SetRelative());
+    void Update() {
+        // 重複ジャンプ防止
+        if (!agent.enabled) {
+            return;
+        }
 
-                // こっちは SetRelative いらない。
-                sequence.Append(transform.DOMoveY(y, 0.25f)
-                    .SetEase(Ease.OutQuad)).OnComplete(() =>
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Jump();
+        }
+    }
+
+    /// <summary>
+    /// ジャンプ
+    /// </summary>
+    private void Jump() {
+        agent.enabled = false;
+        float y = transform.position.y;
+
+        Sequence sequence = DOTween.Sequence();
+        // ジャンプ
+        sequence.Append(transform.DOJump(new Vector3(Input.GetAxis("Horizontal") * 7, 3.0f, Input.GetAxis("Vertical") * 7), jumpPower, 1, 0.5f)
+                .SetRelative());
+
+        // 落下。こっちは SetRelative いらない。
+        sequence.Append(transform.DOMoveY(y, 0.25f)
+                .SetEase(Ease.OutQuad))
+                .OnComplete(() =>
                 {
+                    // 再ジャンプ可能
                     agent.enabled = true;
-                    
+
                     // NavMesh のエリア外(ステージの外側)に出ていないか判定
-                    if (NavMesh.SamplePosition(transform.position, out NavMeshHit hit, 1.0f, NavMesh.AllAreas))
-                    {
+                    if (NavMesh.SamplePosition(transform.position, out NavMeshHit hit, 1.0f, NavMesh.AllAreas)) {
                         // エリア外の場合には最も近い位置 NavMesh の位置に再移動して位置補正
                         transform.position = hit.position;
-                    }
-                });
-                
-                // .OnComplete(() =>
-                // {
-                //     //rb.useGravity = true;
-                //     //DOVirtual.DelayedCall(0.5f, () => agent.enabled = true);
-                //     //rb.DOMoveY(y, 0.5f).SetEase(Ease.OutQuad).SetRelative().OnComplete(() => agent.enabled = true);
-                //     //agent.enabled = true;
-                // });
+                }
             });
     }
 }
